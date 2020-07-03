4b51defd411154b76bc65d095437890f76059b42
Feugang Kemegni Fabrice <fabrigeas@gmail.com>
2020-07-03 07:57:47 +0200

    deploying

---
+++ backend/public/dist/precache-manifest.3199a27f766fba61107adf89ad7c8426.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f9f519db1620116c27a",
+    "url": "/dist/css/app.be3cbefb.css"
+  },
+  {
+    "revision": "ac20a7dd79665b21f4b776b99171c9ce",
+    "url": "/dist/index.html"
+  },
+  {
+    "revision": "ec06dc006204294416ec",
+    "url": "/dist/js/about.26d170ff.js"
+  },
+  {
+    "revision": "9f9f519db1620116c27a",
+    "url": "/dist/js/app.be8c01f2.js"
+  },
+  {
+    "revision": "0cd0fe0ba840493042ac",
+    "url": "/dist/js/chunk-vendors.68bbb5c9.js"
+  },
+  {
+    "revision": "8e80d325f5ddd687b581435adf6253ff",
+    "url": "/dist/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dist/robots.txt"
+  }
+]);
---
+++ backend/public/dist/precache-manifest.dc5052f109d285b9b01a8ce9507f2a95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "967ca24e6f9a1110b527",
+    "url": "/css/app.acbfadc0.css"
+  },
+  {
+    "revision": "bad10b7033ba601f05bff6866f6f8a8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec06dc006204294416ec",
+    "url": "/js/about.26d170ff.js"
+  },
+  {
+    "revision": "967ca24e6f9a1110b527",
+    "url": "/js/app.76c3a331.js"
+  },
+  {
+    "revision": "0cd0fe0ba840493042ac",
+    "url": "/js/chunk-vendors.68bbb5c9.js"
+  },
+  {
+    "revision": "8e80d325f5ddd687b581435adf6253ff",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
